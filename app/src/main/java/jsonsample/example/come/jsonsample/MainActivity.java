a01cc191c016d083c55e2171b8b92ed5cfa7f7c2
unknown <happy_sky@hotmail.com>
2016-10-16 18:18:07 +0800

    Initial commit

---
+++ app/src/main/java/jsonsample/example/come/jsonsample/MainActivity.java
@@ -0,0 +1,15 @@
+package jsonsample.example.come.jsonsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alibaba.fastjson.JSON;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
